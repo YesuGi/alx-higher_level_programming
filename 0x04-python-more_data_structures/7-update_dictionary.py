d40248149ae3a8375b21a2226646b7ac62d86857
YesuGi <yesunehgizaw@gmail.com>
2024-01-10 13:16:26 -0800

    more on data structure

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -7,13 +7,4 @@ def update_dictionary(a_dictionary, key, value):
     key/value in a dictionary.
     """
     a_dictionary[key] = value
-    return a_dictionary#!/usr/bin/python3
-
-
-def update_dictionary(a_dictionary, key, value):
-    """
-    A function that replaces or adds
-    key/value in a dictionary.
-    """
-    a_dictionary[key] = value
     return a_dictionary
